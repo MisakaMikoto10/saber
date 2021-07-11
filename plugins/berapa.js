dacac9123158ffd7b8a515769458ea34ca1018c7
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-11 09:25:29 +0800

    Add files via upload

---
+++ plugins/berapa.js
@@ -0,0 +1,58 @@
+let handler  = async (m, { conn, text }) => {
+  conn.reply(m.chat, `
+*Pertanyaan:* ${m.text}
+*Jawaban:* ${pickRandom(global.brp)}`, m)
+`.trim(), m, m.mentionedJid ? {
+  contextInfo: {
+    mentionedJid: m.mentionedJid
+  }
+} : {})
+}
+handler.help = ['', 'kah'].map(v => 'berapa' + v + ' <text>?')
+handler.tags = ['kerang']
+handler.command = /^berapa(kah)?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
+
+
+global.brp = [
+'1',
+'10',
+'20',
+'30',
+'40',
+'50',
+'60',
+'70',
+'80',
+'99',
+'150',
+'340',
+'423',
+'500',
+'676',
+'780',
+'812',
+'975',
+'1000',
+'Gatau',
+'5678',
+'7500',
+'10000',
+]
---
+++ plugins/botchat.js
@@ -0,0 +1,10 @@
+let handler  = async (m, { conn }) => {
+  
+let name = conn.getName(m.sender)
+let pesan = `Hai ${name}, saya disini\n\nMohon jangan berlebihan dalam memberikan command\n\nDan nama saya Saber bukan Bot`
+conn.reply(m.chat, pesan, m)
+}
+handler.customPrefix = /Bot/
+handler.command = new RegExp
+
+module.exports = handler
