9018f9a3b30baba15281f07b87b53375d78162ae
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-11 09:30:03 +0800

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,10 +1,9 @@
-let handler  = async (m, { conn }) => {
-  
-let name = conn.getName(m.sender)
-let pesan = `Hai ${name}, saya disini\n\nMohon jangan berlebihan dalam memberikan command\n\nDan nama saya Saber bukan Bot`
-conn.reply(m.chat, pesan, m)
-}
-handler.customPrefix = /Bot/
-handler.command = new RegExp
-
+let handler  = async (m, { conn }) => {
+let name = conn.getName(m.sender)
+let pesan = `Hai ${name}, saya disini\n\nMohon jangan berlebihan dalam memberikan command\n\nDan Master Memberikan Saya Nama Saber Bukan Bot`
+conn.reply(m.chat, pesan, m)
+}
+handler.customPrefix = /Bot/
+handler.command = new RegExp
+
 module.exports = handler
