def75d6229f4a60c1746699b4c605a0e32dc234b
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-11 09:32:56 +0800

    Add files via upload

---
+++ plugins/closegroup.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, participants }) => {
+  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+    global.DATABASE._data.chats[m.chat].isBanned = true
+    m.reply('Terima kasih Admin!')
+  // } else m.reply('Ada nomor host disini...')
+}
+handler.help = ['closegroup']
+handler.tags = ['group']
+handler.command = /^closegroup$/i
+handler.group = true
+handler.admin = true
+
+module.exports = handler
