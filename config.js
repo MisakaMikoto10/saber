b6574f8f8aa7133ebe86de109ecac6748d916969
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-11 10:00:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+global.owner = ['62887436568613','6289524339740'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = ['62887436568613','62895361677059'] // Premium user has unlimited limit
+global.wait = "[❗]  ```Wait a minute...```"
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '689330763f16fbcb'
+}
+
+// Sticker WM
+global.packname = 'Saber'
+global.author = 'Rnx'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
